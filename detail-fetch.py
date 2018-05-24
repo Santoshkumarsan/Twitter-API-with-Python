646854d20af05cb8a7ed8eba38149d3bc1b8028c
kashishh2 <kashishh2@gmail.com>
2018-05-25 00:23:55 +0530

    first commit

---
+++ detail-fetch.py
@@ -0,0 +1,8 @@
+
+import twitter
+api = twitter.Api(consumer_key='x**************************',
+  consumer_secret='Rz********************************************',
+    access_token_key='7**************************************************',
+    access_token_secret='c*************************************************')
+
+print(api.VerifyCredentials())
